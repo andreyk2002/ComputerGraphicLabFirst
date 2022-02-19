7cfbfb8096c565f12149b607a11cb1b9204160f1
Andrey <Kuksa>
2022-02-19 19:00:00 +0300

    Added colors converting mechanizm

---
+++ CMYK.py
@@ -0,0 +1,6 @@
+class CMYK:
+    def __init__(self, cyan, magenta, yellow, key):
+        self.cyan = cyan
+        self.magenta = magenta
+        self.yellow = yellow
+        self.key = key
---
+++ HLS.py
@@ -0,0 +1,5 @@
+class HLS:
+    def __init__(self, hue, lightness, saturation):
+        self.hue = hue
+        self.lightness = lightness
+        self.saturation = saturation
